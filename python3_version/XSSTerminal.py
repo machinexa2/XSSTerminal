5c37f03debe2268f1d36a78da751b01242999b66
machinexa <machinexa@gmail.com>
2020-09-30 10:28:57 +0545

    10:28:57 AM 09/30/2020

--- python3_version/XSSTerminal.py
+++ python3_version/XSSTerminal.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import readline
-import urllib.parse
 
 from os import system
 from requests import Session
 from termcolor import colored
 from argparse import ArgumentParser
+from urllib.parse import unquote_plus
 
 from lib.Globals import *
 from lib.Functions import starter, exit_handler
 
-parser = ArgumentParser(description=colored("XSS Terminal", color='yellow'), epilog=colored('<svg onload=alert(1)></svg>', color='yellow'))
+parser = ArgumentParser(description=colored("XSS Terminal", color='yellow'), epilog=colored('<script>window.location="https://bit.ly/3n60FQ4";</script>', color='yellow'))
 string_group = parser.add_mutually_exclusive_group()
-#another = parser.add_mutually_exclusive_group()
 parser.add_argument('-u', '--base-url', type=str, help="Base URL")
 parser.add_argument('-p', '--payload', type=str, help="Starting payload")
 string_group.add_argument('-e', '--error-string', type=str, help="Error string")
-string_group.add_argument('-m', '--match-string', type=str, help="Match string")
-string_group.add_argument('-b', '--blind-string', type=str, help="Blind error")
-parser.add_argument('-M', '--method', type=str, choices=['GET','POST'], help="HTTP Method (Default get)")
+string_group.add_argument('-s', '--match-string', type=str, help="Match string")
+string_group.add_argument('-b', '--blind-string', type=str, help="Blind error string")
+parser.add_argument('-m', '--method', type=str, choices=['GET','POST'], help="HTTP Method (Default get)")
 parser.add_argument('-o', '--output', type=str, help="Output file name")
 parser.add_argument('-r', '--resume', type=str, help="Filename to resume XSST session")
 parser.add_argument('-B', '--banner', action="store_true", help="Print banner and exit")
@@ -44,7 +43,7 @@ class XSST:
         return result
 
     def return_xsscolor(self, xss_payload, joinable) -> str:
-        xssz = urllib.parse.unquote_plus(urllib.parse.unquote_plus(xss_payload)).rstrip(' ')
+        xssz = unquote_plus(unquote_plus(xss_payload)).rstrip(' ')
         if len(joinable) > 1:
             if not "".join(joinable[1:]) in xssz:
                 xss_payload = joinable[0] + colored(xssz, color='red') + "".join(joinable[1:])
@@ -57,25 +56,29 @@ class XSST:
 
     def stringxss_check(self, xss_list) -> str:
         for xssy in xss_list:
-            if urllib.parse.unquote_plus(urllib.parse.unquote_plus(self.xss_payload)) in urllib.parse.unquote_plus(xssy):
-                return xssy
+            if argv.match_string:
+                if unquote_plus(unquote_plus(argv.match_string)) in unquote_plus(xssy):
+                    return xssy
+            else:
+                if unquote_plus(unquote_plus(self.xss_payload)) in unquote_plus(xssy):
+                    return xssy
         return 'WAF Triggered'
 
     def errorxss_check(self, xss_list) -> str:
         for xssy in xss_list:
-            xssz = urllib.parse.unquote_plus(xssy)
-            if not urllib.parse.unquote_plus(argv.error_string) in xssz:
+            xssz = unquote_plus(xssy)
+            if not unquote_plus(argv.error_string) in xssz:
                 return xssy
-            if urllib.parse.unquote_plus(argv.error_string) in xssz:
+            if unquote_plus(argv.error_string) in xssz:
                 return 'WAF Triggered'
         return 'WAF Triggered'
 
     def blindxss_check(self, xss_list) -> str:
         for xssy in xss_list:
-            xssz = urllib.parse.unquote_plus(xssy)
+            xssz = unquote_plus(xssy)
             if urllib.parse.unquote(argv.blind_string) in xssz:
                 return 'WAF Triggered'
-        return 'BLIND'
+        return 'Blind'
 
     def make_xss(self):
         try:
@@ -85,16 +88,20 @@ class XSST:
             xss_list = response.split('\n')
             if argv.error_string:
                 xssy = self.errorxss_check(xss_list)
-            elif argv.payload:
+            elif argv.match_string:
+                xssy = self.stringxss_check(xss_list)
+            elif argv.blind_string: #under development
+                xssy = self.stringxss_check(xss_list)
+            else:
                 xssy = self.stringxss_check(xss_list)
         except Exception as E:
             print(E)
-        if not xssy == 'WAF Triggered':
+        if not xssy == 'WAF Triggered' and not xssy == 'Blind':
             colorful_xss = self.return_xsscolor(self.xss_payload, [xssx for xssx in xssy.strip().split(self.xss_payload) if xssx])
             print(f"{ColorObj.good} {colorful_xss}")
         elif xssy == 'WAF Triggered':
             print(f"{ColorObj.bad} {xssy}")
-        elif xssy == 'BLIND':
+        elif xssy == 'Blind':
             print(f"{ColorObj.good} Successfully executed")
     
 if __name__ == "__main__":
